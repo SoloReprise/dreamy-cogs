b407661896a784b56981516fa8fc270c260e4715
SoloReprise <80115693+SoloReprise@users.noreply.github.com>
2023-07-25 23:19:04 +0200

    Update unitecog.py

--- unitedb/unitecog.py
+++ unitedb/unitecog.py
@@ -21,7 +21,7 @@ class UniteCog(commands.Cog):
 
     def connect(self):
         try:
-            conn = sqlite3.connect(f"Data/callers.db")
+            conn = sqlite3.connect(f"callers.db")
             curs = conn.cursor()
             run = "CREATE TABLE IF NOT EXISTS callers (name varchar PRIMARY KEY, category varchar, text varchar, image varchar);"
             curs.execute(run)
