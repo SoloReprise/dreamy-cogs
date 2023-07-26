b2a31938fc1b5e70a629f4f5e2ce3c46a765fb85
SoloReprise <80115693+SoloReprise@users.noreply.github.com>
2023-07-26 20:58:15 +0200

    Update unitecog.py

--- unitedb/unitecog.py
+++ unitedb/unitecog.py
@@ -23,7 +23,7 @@ class UniteCog(commands.Cog):
         try:
             conn = sqlite3.connect(f"/home/unitedlegacy/.local/share/Red-DiscordBot/data/Spribotito/cogs/CogManager/cogs/unitedb/callers.db")
             curs = conn.cursor()
-            run = "CREATE TABLE IF NOT EXISTS callers (name varchar PRIMARY KEY, category varchar, text varchar, image varchar);"
+            run = "CREATE TABLE IF NOT EXISTS callers (name varchar, category varchar, text varchar, image varchar);"
             curs.execute(run)
             conn.commit()
             return conn, curs
