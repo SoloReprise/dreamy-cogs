4d1138019a9b80885aac8a173d3ee7547aa8cf24
SoloReprise <80115693+SoloReprise@users.noreply.github.com>
2023-08-09 22:08:45 +0200

    Update levelup.py

--- leveluplegacy/levelup.py
+++ leveluplegacy/levelup.py
@@ -1038,7 +1038,7 @@ class LevelUp(UserCommands, Generator, commands.Cog, metaclass=CompositeMetaClas
             description=f"Rank Table:\n{box(data, lang='python')}",
             color=0x70b139,  # Set the embed color to #70b139
         )
-        embed.set_thumbnail(url=ctx.guild.icon_url_as(format="png"))  # Set the server icon as the thumbnail
+        embed.set_thumbnail(url=ctx.guild.icon_url)  # Set the server icon as the thumbnail
 
         ignore = [discord.HTTPException, discord.Forbidden, discord.NotFound]
         if ctx:
