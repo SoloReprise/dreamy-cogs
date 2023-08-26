0e6c446013b3f5a7c3ab9111369e47159da4660c
SoloReprise <80115693+SoloReprise@users.noreply.github.com>
2023-08-26 12:55:49 +0200

    Update unitecog.py

--- unitedb/unitecog.py
+++ unitedb/unitecog.py
@@ -93,9 +93,9 @@ class UniteCog(commands.Cog):
         elif len(matching_records) > 1:
             matching_pokemons = [record[0] for record in matching_records]
             excluded_pokemons = [
-                excluded_keyword for pokemon in matching_pokemons
+                pokemon for pokemon in matching_pokemons
                 for excluded_keyword in excluded_keywords
-                if excluded_keyword in pokemon
+                if excluded_keyword in pokemon.lower()
             ]
 
             # Get the move name from the matching records
@@ -103,8 +103,7 @@ class UniteCog(commands.Cog):
 
             # Remove the excluded keywords from the move name
             for excluded_keyword in excluded_keywords:
-                if excluded_keyword in move_name:
-                    move_name = move_name.replace(excluded_keyword, "").strip()
+                move_name = move_name.replace(excluded_keyword, "").strip()
 
             embed = discord.Embed(
                 title=move_name,
