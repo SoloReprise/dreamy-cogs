97b887a6901bdc083515c90d3cadb2c3b38f36db
SoloReprise <80115693+SoloReprise@users.noreply.github.com>
2023-08-28 21:30:51 +0200

    Update partiditas.py

--- partiditas/partiditas.py
+++ partiditas/partiditas.py
@@ -27,8 +27,8 @@ class Partiditas(commands.Cog):
     @battle.command(name="vs")
     async def vs(self, ctx, role1: discord.Role, role2: discord.Role, num_teams: int = 2, members_per_team: int = 5):
         """Enfrenta a dos roles en equipos y crea canales de voz."""
-        await self._create_teams_and_channels(ctx, role1, role2, num_teams, members_per_team)
-
+        await self._create_teams_and_channels_vs(ctx, role1, role2, num_teams, members_per_team)
+    
     @battle.command(name="clear")
     async def clearscrim(self, ctx):
         """Elimina los canales de voz creados para el enfrentamiento."""
@@ -50,6 +50,58 @@ class Partiditas(commands.Cog):
         await self.config.guild(ctx.guild).user_pairs.set_raw(str(member1.id), value=member2.id)
         await self.config.guild(ctx.guild).user_pairs.set_raw(str(member2.id), value=member1.id)
         await ctx.send(f"¡Los usuarios {member1.mention} y {member2.mention} no estarán en el mismo equipo!")
+
+    async def _create_teams_and_channels(self, ctx, role1: discord.Role, role2: discord.Role = None, num_teams: int = 2, members_per_team: int = 5):
+        guild = ctx.guild
+
+        members_with_role1 = [member.id for member in guild.members if role1 in member.roles]
+        members_with_role2 = [member.id for member in guild.members if role2 in member.roles] if role2 else []
+
+        total_members_needed = num_teams * members_per_team
+
+        if len(members_with_role1) + len(members_with_role2) < total_members_needed:
+            await ctx.send("No hay suficientes miembros con los roles especificados.")
+            return
+
+        members_with_role1 = random.sample(members_with_role1, min(len(members_with_role1), total_members_needed))
+        members_with_role2 = random.sample(members_with_role2, min(len(members_with_role2), total_members_needed))
+
+        combined_members = members_with_role1 + members_with_role2
+        random.shuffle(combined_members)
+
+        # Distribute members into teams
+        teams = [combined_members[i:i+members_per_team] for i in range(0, total_members_needed, members_per_team)]
+
+        # Get the category
+        category = guild.get_channel(1127625556247203861)
+
+        # Create voice channels for each team within the specified category
+        voice_channels = []
+        for index, team in enumerate(teams, start=1):
+            voice_channel_name = f"◇║Equipo {index}"
+            overwrites = {
+                guild.default_role: discord.PermissionOverwrite(connect=False),
+                guild.me: discord.PermissionOverwrite(connect=True)
+            }
+            voice_channel = await category.create_voice_channel(voice_channel_name, overwrites=overwrites)
+            voice_channels.append(voice_channel)
+
+            for member_id in team:
+                member = guild.get_member(member_id)
+                if member.voice:
+                    await member.move_to(voice_channel)
+
+        lista_equipos = []
+        for index, team in enumerate(teams, start=1):
+            miembros_equipo = " ".join([guild.get_member(member_id).mention for member_id in team])
+            lista_equipos.append(f"Equipo {index}: {miembros_equipo}")
+
+        equipos_unidos = "\n".join(lista_equipos)
+        await ctx.send(f"Equipos aleatorizados:\n{equipos_unidos}")
+
+        await self.config.guild(guild).role_to_team.set_raw(str(role1.id), value=teams)
+        if role2:
+            await self.config.guild(guild).role_to_team.set_raw(str(role2.id), value=teams)
 
     async def _create_teams_and_channels_vs(self, ctx, role1: discord.Role, role2: discord.Role, num_teams: int, members_per_team: int):
         guild = ctx.guild
