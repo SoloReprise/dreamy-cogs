dee405f4e7fae592c6732b789ce6ac42ec8e91db
SoloReprise <80115693+SoloReprise@users.noreply.github.com>
2023-08-28 20:33:31 +0200

    Update partiditas.py

--- partiditas/partiditas.py
+++ partiditas/partiditas.py
@@ -13,24 +13,38 @@ class Partiditas(commands.Cog):
         self.config.register_guild(**default_guild)
 
     @commands.command()
+    async def battle(self, ctx):
+        """Comando para gestionar batallas."""
+        pass  # No need to implement anything here as subcommands will handle it
+
+    @battle.command(name="inhouse")
     @commands.guild_only()
     @commands.mod_or_permissions()
-    async def unteam(self, ctx, member1: discord.Member, member2: discord.Member):
-        """Evita que dos usuarios estén en el mismo equipo."""
-        if member1 == member2:
-            await ctx.send("¡No puedes poner a la misma persona en la lista de exclusión!")
-            return
-
-        await self.config.guild(ctx.guild).user_pairs.set_raw(str(member1.id), value=member2.id)
-        await self.config.guild(ctx.guild).user_pairs.set_raw(str(member2.id), value=member1.id)
-        await ctx.send(f"¡Los usuarios {member1.mention} y {member2.mention} no estarán en el mismo equipo!")
-
-    @commands.command()
-    @commands.guild_only()
-    @commands.mod_or_permissions()
-    async def battlers(self, ctx, role: discord.Role, num_teams: int, members_per_team: int):
-        """Randomiza equipos con un rol específico."""
+    async def inhouse_battle(self, ctx, role: discord.Role, num_teams: int = 2, members_per_team: int = 5):
+        """Batalla interna aleatoria."""
         guild = ctx.guild
+        await self._create_teams(ctx, role, num_teams, members_per_team)
+
+    @battle.command(name="vs")
+    @commands.guild_only()
+    @commands.mod_or_permissions()
+    async def vs_battle(self, ctx, role1: discord.Role, role2: discord.Role, num_teams: int = 2, members_per_team: int = 5):
+        """Batalla entre dos roles."""
+        guild = ctx.guild
+        await self._create_teams(ctx, role1, num_teams, members_per_team)
+        await self._create_teams(ctx, role2, num_teams, members_per_team)
+
+    @battle.command(name="clear")
+    @commands.guild_only()
+    @commands.mod_or_permissions()
+    async def clear_battle(self, ctx):
+        """Elimina los canales de voz creados para la batalla."""
+        await self._clear_voice_channels(ctx)
+
+    async def _create_teams(self, ctx, role: discord.Role, num_teams: int, members_per_team: int):
+        """Crea equipos aleatorizados con un rol específico y canales de voz."""
+        guild = ctx.guild
+
         members_with_role = [member.id for member in guild.members if role in member.roles]
 
         total_members_needed = num_teams * members_per_team
@@ -39,23 +53,32 @@ class Partiditas(commands.Cog):
             await ctx.send("No hay suficientes miembros con el rol especificado.")
             return
 
-        random.shuffle(members_with_role)
+        members_with_role = random.sample(members_with_role, min(len(members_with_role), total_members_needed))
 
         # Distribute members into teams
         teams = [members_with_role[i:i+members_per_team] for i in range(0, total_members_needed, members_per_team)]
 
-        # Get user pairs to exclude from the same team
-        user_pairs = await self.config.guild(guild).user_pairs()
-        for member_id, exclusion_id in user_pairs.items():
-            if member_id in members_with_role and exclusion_id in members_with_role:
-                member_team_index = next((i for i, team in enumerate(teams) if member_id in team), None)
-                exclusion_team_index = next((i for i, team in enumerate(teams) if exclusion_id in team), None)
-                if member_team_index is not None and exclusion_team_index is not None and member_team_index == exclusion_team_index:
-                    # Swap the excluded member to another team
-                    target_team = (exclusion_team_index + 1) % num_teams
-                    teams[exclusion_team_index].remove(exclusion_id)
-                    teams[target_team].append(exclusion_id)
+        # Get the category
+        category = guild.get_channel(1127625556247203861)
 
+        # Create voice channels for each team within the specified category
+        voice_channels = []
+        for index, team in enumerate(teams, start=1):
+            voice_channel_name = f"◇║Equipo {index}"
+            overwrites = {
+                guild.default_role: discord.PermissionOverwrite(connect=False),
+                guild.me: discord.PermissionOverwrite(connect=True)
+            }
+            voice_channel = await category.create_voice_channel(voice_channel_name, overwrites=overwrites)
+            voice_channels.append(voice_channel)
+
+            for member_id in team:
+                member = guild.get_member(member_id)
+                if member.voice:
+                    await member.move_to(voice_channel)
+
+        await self._handle_user_pairs(ctx, role, teams)
+        
         lista_equipos = []
         for index, team in enumerate(teams, start=1):
             miembros_equipo = " ".join([guild.get_member(member_id).mention for member_id in team])
@@ -65,3 +88,29 @@ class Partiditas(commands.Cog):
         await ctx.send(f"Equipos aleatorizados:\n{equipos_unidos}")
 
         await self.config.guild(guild).role_to_team.set_raw(str(role.id), value=teams)
+
+    async def _handle_user_pairs(self, ctx, role, teams):
+        """Maneja las exclusiones de usuarios en el mismo equipo."""
+        guild = ctx.guild
+        user_pairs = await self.config.guild(guild).user_pairs()
+        members_with_role = [member.id for member in guild.members if role in member.roles]
+
+        for member_id, exclusion_id in user_pairs.items():
+            if member_id in members_with_role and exclusion_id in members_with_role:
+                member_team_index = next((i for i, team in enumerate(teams) if member_id in team), None)
+                exclusion_team_index = next((i for i, team in enumerate(teams) if exclusion_id in team), None)
+                if member_team_index is not None and exclusion_team_index is not None and member_team_index == exclusion_team_index:
+                    # Swap the excluded member to another team
+                    target_team = (exclusion_team_index + 1) % len(teams)
+                    teams[exclusion_team_index].remove(exclusion_id)
+                    teams[target_team].append(exclusion_id)
+
+    async def _clear_voice_channels(self, ctx):
+        """Elimina los canales de voz creados para la batalla."""
+        guild = ctx.guild
+        voice_channels = [channel for channel in guild.voice_channels if "◇║Equipo" in channel.name]
+
+        for channel in voice_channels:
+            await channel.delete()
+
+        await ctx.send("Canales de voz de batalla eliminados.")
