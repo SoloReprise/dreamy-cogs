f93d770c02cb10b2e623c6cc3dec7ab3b911d442
SoloReprise <80115693+SoloReprise@users.noreply.github.com>
2023-08-31 12:54:08 +0200

    Update partiditas.py

--- partiditas/partiditas.py
+++ partiditas/partiditas.py
@@ -87,9 +87,8 @@ class Partiditas(commands.Cog):
 
         total_members_needed = num_teams * members_per_team
 
-        combined_members = set(members_with_role1 + members_with_role2)
-        if len(combined_members) < num_teams:
-            await ctx.send("No hay suficientes miembros con los roles especificados.")
+        if len(set(members_with_role1)) < num_teams or len(set(members_with_role2)) < num_teams:
+            await ctx.send("No hay suficientes miembros con los roles especificados para crear ese número de equipos.")
             return
 
         unique_members_with_role1 = list(set(members_with_role1))
