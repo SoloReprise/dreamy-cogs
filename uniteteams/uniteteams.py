47b822a9a1c17fd8b54837b2a42019f69e9d9408
SoloReprise <80115693+SoloReprise@users.noreply.github.com>
2023-09-04 12:00:47 +0200

    Update uniteteams.py

--- uniteteams/uniteteams.py
+++ uniteteams/uniteteams.py
@@ -4,33 +4,35 @@ from redbot.core import commands, Config
 class UniteTeams(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.config = Config.get_conf(self, identifier=1234567890)
-        self.config.register_guild(teams={})
+        self.config.register_guild(uniteteams={})
 
     @commands.guild_only()
     @commands.command()
-    async def uniteteams(self, ctx, subcommand: str, leader: discord.Member = None, *, team_name: str = None):
+    async def uniteteams(self, ctx, subcommand: str, leader_or_role: discord.Member=None, *, team_name: str = None):
         if ctx.guild.owner != ctx.author:
             await ctx.send("¡Solo el dueño del servidor puede usar este comando!")
             return
-        
+
         if subcommand == "create":
-            if not leader or not team_name:
+            if not leader_or_role or not team_name:
                 await ctx.send("¡Por favor, menciona a un líder y proporciona un nombre de equipo!")
                 return
-            
-            await self.create_team(ctx, leader, team_name)
+            await self.create_team(ctx, leader_or_role, team_name)
+
         elif subcommand == "delete":
-            await self.delete_team(ctx, team_name)  # Assuming team_name here will be a role        elif subcommand == "list":
+            await self.delete_team(ctx, leader_or_role)  # Assuming leader_or_role here will be a role
+
+        elif subcommand == "list":
             await self.list_teams(ctx)
 
         elif subcommand == "clean":
             await self.clean_teams(ctx)
+
         else:
             await ctx.send("Subcomando desconocido.")
 
     async def create_team(self, ctx, leader: discord.Member, team_name: str):
-        if isinstance(leader, str):
+        if not isinstance(leader, discord.Member):
             await ctx.send("¡Por favor menciona a un líder válido!")
             return
 
@@ -52,39 +54,39 @@ class UniteTeams(commands.Cog):
 
         await leader.add_roles(role, scrims_role, capitanes_role)
 
-        await self.config.guild(ctx.guild).teams.set_raw(team_name, value={"role_id": role.id, "leader_id": leader.id})
+        await self.config.guild(ctx.guild).uniteteams.set_raw(team_name, value={"role_id": role.id, "leader_id": leader.id})
         await ctx.send(f"¡Equipo {team_name} creado con {leader.mention} como líder!")
 
     async def delete_team(self, ctx, team_role: discord.Role):
-        team_data = await self.config.guild(ctx.guild).teams.get_raw(team_role.name, default=None)
+        team_data = await self.config.guild(ctx.guild).uniteteams.get_raw(team_role.name, default=None)
         if not team_data:
             await ctx.send("¡Este equipo no existe!")
             return
 
         await team_role.delete()
 
-        await self.config.guild(ctx.guild).teams.clear_raw(team_role.name)
+        await self.config.guild(ctx.guild).uniteteams.clear_raw(team_role.name)
         await ctx.send(f"¡Equipo {team_role.name} eliminado!")
 
     async def list_teams(self, ctx):
-        teams_data = await self.config.guild(ctx.guild).teams()
+        teams_data = await self.config.guild(ctx.guild).uniteteams()
         if not teams_data:
             await ctx.send("¡No hay equipos creados!")
             return
-        
+
         message = "Equipos:\n"
         for team, data in teams_data.items():
             leader = ctx.guild.get_member(data["leader_id"])
             message += f"- {team} (Líder: {leader.mention if leader else 'Desconocido'})\n"
-        
+
         await ctx.send(message)
 
     async def clean_teams(self, ctx):
-        teams_data = await self.config.guild(ctx.guild).teams()
+        teams_data = await self.config.guild(ctx.guild).uniteteams()
         for team, data in teams_data.items():
             role = ctx.guild.get_role(data["role_id"])
             if role:
                 await role.delete()
 
-        await self.config.guild(ctx.guild).teams.clear()
+        await self.config.guild(ctx.guild).uniteteams.clear()
         await ctx.send("¡Todos los equipos han sido eliminados!")
