185c29d415d2ac3264308ae096a37012670f74eb
SoloReprise <80115693+SoloReprise@users.noreply.github.com>
2023-09-05 23:32:15 +0200

    Update converter.py

--- whosthatpokemon/converter.py
+++ whosthatpokemon/converter.py
@@ -1,33 +1,33 @@
 from random import randint
-
 from redbot.core import commands
 
-
 class Generation(commands.Converter):
+    """
+    A converter for the RedBot that converts Pokémon generation strings to 
+    a random number within that generation's range of Pokémon.
+    """
+    
+    GEN_MAP = {
+        "gen1": (1, 151),
+        "gen2": (152, 251),
+        "gen3": (252, 386),
+        "gen4": (387, 493),
+        "gen5": (494, 649),
+        "gen6": (650, 721),
+        "gen7": (722, 809),
+        "gen8": (810, 898),
+        "arceus": (899, 905),
+        "gen9": (906, 1010),
+    }
+
     async def convert(self, ctx: commands.Context, argument: str) -> int:
-        allowed_gens = [f"gen{x}" for x in range(1, 11)] + ["arceus"]
+        argument = argument.lower()
         
-        if argument.lower() not in allowed_gens:
+        if argument not in self.GEN_MAP:
             ctx.command.reset_cooldown(ctx)
-            raise commands.BadArgument("Only `gen1` to `gen8`, `arceus`, and `gen9` values are allowed.")
+            raise commands.BadArgument(
+                "Only `gen1` to `gen10`, and `arceus` values are allowed."
+            )
         
-        if argument.lower() == "gen1":
-            return randint(1, 151)
-        elif argument.lower() == "gen2":
-            return randint(152, 251)
-        elif argument.lower() == "gen3":
-            return randint(252, 386)
-        elif argument.lower() == "gen4":
-            return randint(387, 493)
-        elif argument.lower() == "gen5":
-            return randint(494, 649)
-        elif argument.lower() == "gen6":
-            return randint(650, 721)
-        elif argument.lower() == "gen7":
-            return randint(722, 809)
-        elif argument.lower() == "gen8":
-            return randint(810, 898)
-        elif argument.lower() == "arceus":
-            return randint(899, 905)
-        elif argument.lower() == "gen9":
-            return randint(906, 1010)
+        start, end = self.GEN_MAP[argument]
+        return randint(start, end)
