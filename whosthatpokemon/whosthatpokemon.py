15f8f57d045fbc6880a39682ca7f61f5241a009e
SoloReprise <80115693+SoloReprise@users.noreply.github.com>
2023-09-06 14:16:06 +0200

    Update whosthatpokemon.py

--- whosthatpokemon/whosthatpokemon.py
+++ whosthatpokemon/whosthatpokemon.py
@@ -175,7 +175,7 @@ class WhosThatPokemon(commands.Cog):
 
         usage_count = await self.config.user(ctx.author).usage_count() or 0
 
-        if usage_count >= 5:
+        if usage_count >= 10:
             # Calculate the remaining time until 9 am GMT+2 (or 7 am UTC)
             remaining_time = (datetime.combine(datetime.now().date(), time(7, 0)) + timedelta(days=1)) - datetime.now()
             hours, remainder = divmod(remaining_time.seconds, 3600)
