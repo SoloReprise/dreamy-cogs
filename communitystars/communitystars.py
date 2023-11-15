fc8cd1b015e738a00c68cc245ceab00fe38b08a7
SoloReprise <80115693+SoloReprise@users.noreply.github.com>
2023-11-15 14:53:53 +0100

    Update communitystars.py

--- communitystars/communitystars.py
+++ communitystars/communitystars.py
@@ -4,16 +4,23 @@ import discord
 class CommunityStars(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.config = Config.get_conf(self, identifier=1288867890)
+        self.config = Config.get_conf(self, identifier=1234567890)
         self.config.register_global(reactions={})
 
-        # Define the categories in which the cog should not work
-        self.excluded_categories = [1127784491625234432, 1127625556247203860]
+        # Define the categories and their exceptions
+        self.excluded_categories = {
+            1127625556247203860: [1139134195524960276],  # Allow this channel in the category
+            1127784491625234432: [1127798424864034846]  # Allow this channel in the category
+        }
 
     @commands.Cog.listener()
     async def on_reaction_add(self, reaction, user):
-        # Check if the channel's category is not in the excluded list
-        if reaction.message.channel.category_id not in self.excluded_categories:
+        channel = reaction.message.channel
+        category_id = channel.category_id
+        channel_id = channel.id
+
+        # Check if the channel's category is not in the excluded list or it's an allowed channel
+        if category_id not in self.excluded_categories or channel_id in self.excluded_categories.get(category_id, []):
             # Check if the reaction is a star and the message is not already pinned
             if str(reaction.emoji) == "⭐" and not reaction.message.pinned:
                 message_id = reaction.message.id
