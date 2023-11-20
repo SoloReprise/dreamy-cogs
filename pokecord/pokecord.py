8b0e0aa4a388066661c1918ef3483615b4139f3b
SoloReprise <80115693+SoloReprise@users.noreply.github.com>
2023-11-20 20:41:56 +0100

    Update pokecord.py

--- pokecord/pokecord.py
+++ pokecord/pokecord.py
@@ -47,11 +47,6 @@ class Pokecord(
     metaclass=CompositeMetaClass,
 ):
 
-    def format_help_for_context(self, ctx):
-        """Thanks Sinbad."""
-        pre_processed = super().format_help_for_context(ctx)
-        return f"{pre_processed}\nAuthor: {self.__author__}\nCog Version: {self.__version__}"
-
     def __init__(self, bot):
         super().__init__()
         self.bot = bot
