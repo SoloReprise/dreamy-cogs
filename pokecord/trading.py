6e2bf520e9199084fd2c9cb7882acc2f01954b36
SoloReprise <80115693+SoloReprise@users.noreply.github.com>
2023-11-21 19:12:19 +0100

    Update trading.py

--- pokecord/trading.py
+++ pokecord/trading.py
@@ -1,5 +1,6 @@
 import asyncio
 import json
+import random
 
 import discord
 from redbot.core.utils.chat_formatting import box
@@ -79,3 +80,43 @@ class TradeMixin(MixinMeta):
             )
         else:
             await ctx.send("Intercambio cancelado.")
+
+    @poke.command(usage="<ID de tu pokémon>")
+    async def wondertrade(self, ctx, pokemon_id: int):
+        """Intercambia tu Pokémon por uno aleatorio."""
+        async with ctx.typing():
+            # Obtener tu Pokémon
+            result = await self.cursor.fetch_all(
+                query="SELECT pokemon, message_id FROM users WHERE user_id = :user_id",
+                values={"user_id": ctx.author.id},
+            )
+            your_pokemons = [None]
+            for data in result:
+                your_pokemons.append([json.loads(data[0]), data[1]])
+
+            if not your_pokemons or pokemon_id >= len(your_pokemons):
+                return await ctx.send("No tienes un Pokémon con ese ID.")
+
+            your_pokemon = your_pokemons[pokemon_id]
+
+            # Generar un Pokémon aleatorio
+            random_pokemon = self.pokemon_choose()
+            random_pokemon["level"] = random.randint(1, 100)  # Ejemplo de nivel aleatorio
+
+            # Intercambio
+            await self.cursor.execute(
+                "DELETE FROM users WHERE message_id = :message_id",
+                values={"message_id": your_pokemon[1]}
+            )
+            await self.cursor.execute(
+                query=INSERT_POKEMON,
+                values={
+                    "user_id": ctx.author.id,
+                    "message_id": ctx.message.id,
+                    "pokemon": json.dumps(random_pokemon),
+                },
+            )
+
+            await ctx.send(
+                f"Has intercambiado tu {self.get_name(your_pokemon[0]['name'], ctx.author)} por un {self.get_name(random_pokemon['name'], ctx.author)} nivel {random_pokemon['level']}."
+            )
