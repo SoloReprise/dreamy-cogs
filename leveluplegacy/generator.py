ea1c0550924628c26db6b4951769d0f7ca7197f0
SoloReprise <80115693+SoloReprise@users.noreply.github.com>
2024-01-29 20:52:43 +0100

    Update generator.py

--- leveluplegacy/generator.py
+++ leveluplegacy/generator.py
@@ -701,8 +701,8 @@ class Generator(MixinMeta, ABC):
             _("Experiencia: ")
             + f"{humanize_number(user_xp_progress)}/{humanize_number(next_xp_diff)} ({humanize_number(user_xp)} total)"
         )
-        message_count = ""
-        voice = ""  
+        messages = _("Mensajes: ") + str(messages)
+        voice = _("Tiempo en voz: ") + str(voice)
         stars = str(stars)
         bal = _("Balance: ") + f"{humanize_number(balance)} {currency}"
         prestige_str = _("Prestige ") + str(prestige)
