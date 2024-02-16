b06f36f0647aca9ed61a3bb540d70c834b1506e0
SoloReprise <80115693+SoloReprise@users.noreply.github.com>
2024-02-16 20:42:12 +0100

    Putas comas

--- leveluplegacy/base.py
+++ leveluplegacy/base.py
@@ -1147,7 +1147,7 @@ class UserCommands(MixinMeta, ABC):
                     },
                     "font_name": p.get("font"),
                     "render_gifs": p.get("render_gifs", False),
-                    "blur": p.get("blur", False)
+                    "blur": p.get("blur", False),
                     "pokedex": p.get("pokedex", [])  # Assuming p["pokedex"] contains the list of Pokémon names
                 }
 
